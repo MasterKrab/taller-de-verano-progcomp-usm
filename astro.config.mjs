bae3ddf76f22d684ff664e847e65934a156209e1
Enzo Vivallo <enzovivallo@estudiante.uc.cl>
2025-12-07 13:55:35 -0300

    feat(SEO): add meta tags and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
 import { defineConfig } from 'astro/config'
 
-import icon from 'astro-icon';
+import icon from 'astro-icon'
 
 // https://astro.build/config
 export default defineConfig({
