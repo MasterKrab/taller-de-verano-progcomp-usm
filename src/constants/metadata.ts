ef39c0ef009e50f22a4d645df8c94d9a8d3de6d1
Enzo Vivallo <enzovivallo@estudiante.uc.cl>
2025-12-07 01:53:43 -0300

    fix: fix constants file an remove unused page

---
+++ src/constants/metadata.ts
@@ -0,0 +1,3 @@
+export const SITE_DESCRIPTION = 'Taller de Verano de Programación Competitiva'
+export const SITE_TITLE =
+  'Taller de verano en la Universidad Técnica Federico Santa María donde aprenderás programación competitiva desde cero'
--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,13 +1,14 @@
 import { getCollection } from 'astro:content'
 import rss from '@astrojs/rss'
-import { SITE_DESCRIPTION, SITE_TITLE } from '../consts'
+import { SITE_DESCRIPTION, SITE_TITLE } from '@constants/metadata'
+import type { APIContext } from 'astro'
 
-export async function GET(context) {
+export async function GET(context: APIContext) {
   const posts = await getCollection('posts')
   return rss({
     title: SITE_TITLE,
     description: SITE_DESCRIPTION,
-    site: context.site,
+    site: context.site as URL,
     items: posts.map((post) => ({
       ...post.data,
       link: `/posts/${post.id}/`,
